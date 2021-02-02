6babc8046649cc2fc5632d303ab7ca40933a296d
trickered <brrumur476@gmail.com>
2021-02-01 18:49:46 -0600

    se sube los arreglos de no recibir parametros en metodos no recursivos

--- BL/ArbolBinario.py
+++ BL/ArbolBinario.py
@@ -1,4 +1,15 @@
+from BL.Nodo import Nodo
+
+
 class ArbolBinario(object):
+
+    def __init__(self):
+        self.root=None
+
+    def inicializarRoot(self, val):
+        if self.root == None:
+            nodo = Nodo(val)
+            self.root=nodo
 
     def insert(self,root, node):
         if root is None:
@@ -11,28 +22,28 @@ class ArbolBinario(object):
 
     def inOrdenRecursivo(self, root):
         if not root:
-            return "-"
+            return ""
         else:
-            return self.inOrdenRecursivo(root.left)+str(root.info)+self.inOrdenRecursivo(root.right)
+            return self.inOrdenRecursivo(root.left)+str(root.info)+","+self.inOrdenRecursivo(root.right)
 
 
-    def inOrden(self, root):
-        return self.inOrdenRecursivo(root)
+    def inOrden(self):
+        return self.inOrdenRecursivo(self.root)
 
     def preOrdenRecursivo(self, root):
         if not root:
-            return "-"
+            return ""
         else:
-            return str(root.info)+self.preOrdenRecursivo(root.left)+self.preOrdenRecursivo(root.right)
+            return str(root.info)+","+self.preOrdenRecursivo(root.left)+self.preOrdenRecursivo(root.right)
 
-    def preOrden(self, root):
-        return self.preOrdenRecursivo(root)
+    def preOrden(self):
+        return self.preOrdenRecursivo(self.root)
 
     def postOrdenRecursivo(self, root):
         if not root:
             return ""
         else:
-            return self.postOrdenRecursivo(root.left)+self.postOrdenRecursivo(root.right)+ str(root.info)
+            return self.postOrdenRecursivo(root.left)+self.postOrdenRecursivo(root.right)+ str(root.info)+","
 
-    def postOrden(self, root):
-        return self.postOrdenRecursivo(root)
+    def postOrden(self):
+        return self.postOrdenRecursivo(self.root)
--- CL/ArbolController.py
+++ CL/ArbolController.py
@@ -5,22 +5,21 @@ from BL.Nodo import Nodo
 class ArbolController(object):
     def __init__(self):
         self.arbol = ArbolBinario()
-        self.root = None
         self.cont = 0
 
     def agregarNodo(self, info):
         nodo = Nodo(info)
         if self.cont == 0:
-            self.root = self.arbol.insert(self.root, nodo)
+            self.arbol.inicializarRoot(info)
             self.cont = self.cont+1
         else:
-            self.arbol.insert(self.root, nodo)
+            self.arbol.insert(self.arbol.root, nodo)
 
     def obtenerInOrden(self):
-        return self.arbol.inOrden(self.root)
+        return self.arbol.inOrden()
 
     def obtenerPreOrden(self):
-        return self.arbol.preOrden(self.root)
+        return self.arbol.preOrden()
 
     def obtenerPostOrden(self):
-        return self.arbol.postOrden(self.root)
+        return self.arbol.postOrden()
